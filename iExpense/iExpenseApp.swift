dca2eddee0e2d6ad4ba0b678bffd8d4a53f2b9e1
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-11 11:54:26 +0000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Yashavika Singh on 11.06.24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
